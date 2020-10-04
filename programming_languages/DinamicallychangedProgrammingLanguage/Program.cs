e4e2fd0580b374243f70fcc31187e95e4aacd3f0
Arturas <info@webai.lt>
2020-10-05 00:38:09 +0300

    programming languages add

---
+++ programming_languages/DinamicallychangedProgrammingLanguage/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Chain<T> : List<Func<T, T>>
+{
+    public T Evaluate(T input)
+    {
+        foreach (var ix in this)
+        {
+            input = ix(input);
+        }
+        return input;
+    }
+}
+
+namespace DinamicallychangedProgrammingLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var keywords = new Dictionary<string, Func<string, string>>
+            {
+                ["capitalize"] = (input) =>
+                {
+                    return input.Replace("e", "EE");
+                },
+                ["replace"] = (input) =>
+                {
+                    return input.Replace("o", "O");
+                }
+            };
+
+            string code = "capitalize replace";
+            var tokens = code.Split(' ');
+            var chain = new Chain<string>();
+            chain.AddRange(tokens.Select(ix => keywords[ix]));
+            var result = chain.Evaluate("join the revolution, capitalize and replace");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ programming_languages/ScriptingLanguage/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScriptingLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var keywords = new Dictionary<string, Action>
+            {
+                ["hello"] = () =>
+                {
+                    Console.WriteLine("Hello was invoked");
+                },
+                ["world"] = () =>
+                {
+                    Console.WriteLine("|world was invoked");
+                }
+            };
+
+            string code = "hello world";
+            var tokens = code.Split(' ');
+
+            foreach(var token in tokens)
+            {
+                keywords[token]();
+            }
+        }
+    }
+}
